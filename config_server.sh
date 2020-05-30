5770472d269ed597c53414db293e8930d0e874c6
cavalen <cavalen@hotmail.com>
2020-05-29 19:55:44 -0500

    Update config_server.sh

--- config_server.sh
+++ config_server.sh
@@ -9,10 +9,10 @@ echo -e "Docker, Ansible Installed"
 # Run containers
 sudo docker run --name hackazon --restart unless-stopped -d -p 8080:80 -p 8443:443 ianwijaya/hackazon
 sudo docker run --name dvwa --restart unless-stopped -d -p 8081:80 vulnerables/web-dvwa
-sudo docker run --name f5helloworld --restart unless-stopped -d -p 8082:8080 f5devcentral/f5-hello-world 
+sudo docker run --name bwaap --restart=unless-stopped -d -p 8082:80 raesene/bwapp
 sudo docker run --name juice-shop --restart=unless-stopped -d -p 8083:3000 bkimminich/juice-shop
-sudo docker run --name nginx01 --restart=unless-stopped -d -p 8084:80 nginx:latest
-sudo docker run --name nginx02 --restart=unless-stopped -d -p 8085:80 nginx:latest
+sudo docker run --name f5helloworld --restart unless-stopped -d -p 8084:8080 f5devcentral/f5-hello-world 
+sudo docker run --name nginx01 --restart=unless-stopped -d -p 8085:80 nginx:latest
 
 # Arcadia 
 #sudo docker network create internal
