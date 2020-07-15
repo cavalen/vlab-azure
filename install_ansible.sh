91e937ad6c71809d8434491b580b739038b98db7
cavalen <cavalen@hotmail.com>
2020-07-15 17:04:44 -0500

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,7 +2,7 @@
 # Install Ansible & Dependencies
 sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt update
-sudo apt install -y software-properties-common ansible docker.io docker-compose python3-pip
+sudo apt install -y software-properties-common ansible docker.io docker-compose python3-pip rpm
 # Ubuntu 20.04 > sudo apt install -y software-properties-common ansible python3-pip azure-cli
 pip3 install boto boto3 netaddr passlib bigsuds deepdiff 'ansible[azure]' 
 ansible-galaxy install f5devcentral.f5app_services_package
