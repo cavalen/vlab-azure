427b6ba2c54db5fbc5daf0280a1325c42f7c075f
cavalen <cavalen@hotmail.com>
2020-07-16 19:34:07 -0500

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,9 +1,8 @@
 # For Ubuntu 18.04 LTS - Install Ansible & Dependencies
 sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt update
-sudo apt install -y software-properties-common ansible docker.io docker-compose python3-pip rpm
-# Ubuntu 20.04 > sudo apt install -y software-properties-common ansible python3-pip azure-cli
-pip3 install boto boto3 netaddr passlib bigsuds deepdiff 'ansible[azure]' 
+sudo apt install -y software-properties-common ansible python3-pip rpm
+sudo pip3 install boto boto3 netaddr passlib bigsuds deepdiff 'ansible[azure]' 
 ansible-galaxy install f5devcentral.f5app_services_package
 
 # Install Azure CLI 
